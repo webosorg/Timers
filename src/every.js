25e3ea0cd1493d9189da6047d76c53a699823621
Suren Atoyan <s.atoyan@apollobytes.com>
2017-12-17 03:19:01 +0400

    Change 'Function Declaration' to 'Arrow Function'

--- src/every.js
+++ src/every.js
@@ -2,7 +2,7 @@ import setImmediate, { clearImmediate } from './setImmediate';
 
 import ReusableLazyPromise from './utils/reusableLazyPromise';
 
-function every(fn, time = 0, iterations = Infinity) {
+const every = (fn, time = 0, iterations = Infinity) => {
   const rPromise = new ReusableLazyPromise(res => {
     const timerID = setInterval(
       _ => iterations-- > 0 ? fn() : (rPromise.stop() & res()), time
